d1be1de4572d47052f3ce71c1d66a5e87c998790
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-09 01:13:10 +0430

    set table with beautifultable and scrap the covid-19 site

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import re
 import time
 from bs4 import BeautifulSoup as bs
 from beautifultable import BeautifulTable
+
+table = BeautifulTable()
+country = input("\n Enter Country: ").lower()
+url = "https://www.worldometers.info/coronavirus/country/" + country
+req = requests.get(url)
+scrap = bs(req.content , "html.parser")
+data = scrap.find_all("div" , {"class" : "maincounter-number"} , "span")
+list = []
